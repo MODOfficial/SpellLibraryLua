b77dcde66ee8ed9842313340672795e5ace9db55
HappyFeedFriends <31066449+HappyFeedFriends@users.noreply.github.com>
2020-05-12 15:19:26 +0500

    refactoring

--- game/scripts/vscripts/heroes/abaddon/borrowed_time.lua
+++ game/scripts/vscripts/heroes/abaddon/borrowed_time.lua
@@ -21,40 +21,32 @@ modifier_abaddon_borrowed_time_lua_active = class({
     GetEffectName           = function(self) return 'particles/units/heroes/hero_abaddon/abaddon_borrowed_time.vpcf' end,
     GetEffectAttachType     = function(self) return PATTACH_ABSORIGIN_FOLLOW end,
     StatusEffectPriority    = function(self) return MODIFIER_PRIORITY_HIGH end,
-
-    OnCreated               = function(self)
-        self.caster = self:GetCaster()
-        self.parent = self:GetParent()
-        -- self.radius = self:GetAbility():GetSpecialValueFor('redirect_range_scepter')
-        self.caster:Purge(false, true, true, true, false)
-        -- if self.caster:HasScepter() and self.caster:HasAbility('ability_mist_coil') then 
-        --     self.ability_mist_coil = self.caster:FindAbilityByName('ability_mist_coil')
-        --     self.TargetType = self.ability_mist_coil:GetAbilityTargetType()
-        --     self.TargetFlags = self.ability_mist_coil:GetAbilityTargetFlags()
-        --     -- self:StartIntervalThink(0.1)
-        -- end
-    end,
-
-    OnIntervalThink          = function(self)
-        if IsClient() then return end
-
-        local units = FindUnitsInRadius(self.caster:GetTeam(), 
-        self.parent:GetOrigin(), 
-        nil, 
-        self.radius,
-        DOTA_UNIT_TARGET_TEAM_FRIENDLY, 
-        self.TargetType, 
-        self.TargetFlags,
-        FIND_ANY_ORDER, 
-        false)
-
-        for k,v in pairs(units) do 
-            if v ~= self.caster then 
-                self.caster:SetCursorCastTarget(v)
-                self.ability_mist_coil:OnSpellStart()
-            end
-        end
-        
-    end,
 })
+
+function modifier_abaddon_borrowed_time_lua_active:OnCreated()
+    self.caster = self:GetCaster()
+    self.parent = self:GetParent()
+    self.caster:Purge(false, true, true, true, false)
+end
+
+function modifier_abaddon_borrowed_time_lua_active:OnIntervalThink()
+    if IsClient() then return end
+
+    local units = FindUnitsInRadius(self.caster:GetTeam(), 
+    self.parent:GetOrigin(), 
+    nil, 
+    self.radius,
+    DOTA_UNIT_TARGET_TEAM_FRIENDLY, 
+    self.TargetType, 
+    self.TargetFlags,
+    FIND_ANY_ORDER, 
+    false)
+
+    for k,v in pairs(units) do 
+        if v ~= self.caster then 
+            self.caster:SetCursorCastTarget(v)
+            self.ability_mist_coil:OnSpellStart()
+        end
+    end
+end
 
